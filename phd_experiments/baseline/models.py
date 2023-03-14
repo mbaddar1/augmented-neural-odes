c7b008678c78f14b49f6759545069933a88591b4
mbaddar <mbaddar2@gmail.com>
2023-03-14 15:19:19 +0100

    add more code to baseline

--- phd_experiments/baseline/models.py
+++ phd_experiments/baseline/models.py
@@ -4,6 +4,8 @@ https://d2l.ai/chapter_convolutional-modern/resnet.html
 https://debuggercafe.com/implementing-resnet18-in-pytorch-from-scratch/
 https://blog.paperspace.com/writing-resnet-from-scratch-in-pytorch/
 """
+import logging
+
 import numpy as np
 import sklearn.model_selection
 import torch.nn
@@ -11,25 +13,27 @@ from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error
 from torch.nn import Sequential, MSELoss
 from torch.utils.data import DataLoader
-
+from tqdm import tqdm
 from phd_experiments.datasets.torch_sklearn_diabetes import TorchDiabetesDataset
 from phd_experiments.datasets.toy_linear import ToyLinearDataSet1
 from phd_experiments.datasets.toy_relu import ToyRelu
 
 
-def build_sklearn_diabetes_baseline():
+def build_sklearn_diabetes_baseline(X: np.ndarray, y: np.ndarray, loss_function_name: str):
     # https://scikit-learn.org/stable/auto_examples/linear_model/plot_ols.html#sphx-glr-auto-examples-linear-model-plot-ols-py
     epochs = 100
-    mse_list = []
+    loss_vals_list = []
     for i in range(epochs):
-        X, y = datasets.load_diabetes(return_X_y=True)
         X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.33)
         regr = linear_model.LinearRegression()
         regr.fit(X_train, y_train)
         y_pred = regr.predict(X_test)
-        mse = mean_squared_error(y_test, y_pred)
-        mse_list.append(mse)
-    ret = {'model': str(regr.__class__), 'avg-mse': np.nanmean(mse_list), 'std-mse': np.nanstd(mse_list)}
+        if loss_function_name == 'mse':
+            loss_val = mean_squared_error(y_test, y_pred)
+            loss_vals_list.append(loss_val)
+
+    ret = {'model': str(regr.__class__), 'loss_function_name': loss_function_name,
+           'avg-loss': np.nanmean(loss_vals_list), 'std-loss': np.nanstd(loss_vals_list)}
     return ret
 
 
@@ -126,28 +130,36 @@ class ResNetBasic(torch.nn.Module):
         return self.model(x)
 
 
-if __name__ == '__main__':
+def get_torch_loss(loss_function_name):
+    if loss_function_name == 'mse':
+        return MSELoss()
+    else:
+        raise ValueError(F"Unknown loss name {loss_fn}")
 
-    batch_size = 32
-    epochs = 1000
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger()
+    ###
+    batch_size = 64
+    epochs = 10000
     N = None
     input_dim = None
     output_dim = None
     hidden_dim = 64
     n_layers = 5
-    activation_function_name = 'identity'
-    lr = 0.1
-    mse_loss_fn = MSELoss()
-    dataset = 'diabetes'
-    model = 'linear'
-    baselines = {}
+    activation_function_name = 'relu'
+    lr = 1e-3
+
+    dataset = 'toy-relu'
+    model = 'nn'
+    baseline = None
+    loss_function_name = 'mse'
     ###
     if dataset == 'diabetes':
         input_dim = 10
         output_dim = 1
         ds = TorchDiabetesDataset()
-        ret = build_sklearn_diabetes_baseline()
-        print(f'diabetes baseline model (linear-regression) score = {ret}')
 
     elif dataset == 'toy-linear-1':
         # TODO The nn models works fine with out-dim=1, higher dims (Multi-linear regression, No..) . Dig Deeper
@@ -168,8 +180,11 @@ if __name__ == '__main__':
     else:
         raise ValueError(f'unknown dataset {dataset}')
 
+    # build baseline
+    baseline = build_sklearn_diabetes_baseline(X=ds.X.detach().numpy(), y=ds.y.detach().numpy(),
+                                               loss_function_name=loss_function_name)
     dl = DataLoader(dataset=ds, batch_size=batch_size, shuffle=True)
-    if model == 'baseline':
+    if model == 'resnet':
         model = ResNetBasic(n_layers=n_layers, input_dim=input_dim, hidden_dim=hidden_dim,
                             output_dim=output_dim, activation_function_name=activation_function_name)
     elif model == 'nn':
@@ -179,12 +194,27 @@ if __name__ == '__main__':
         model = LinearModel(input_dim=input_dim, out_dim=output_dim)
 
     optimizer = torch.optim.Adam(params=model.parameters(), lr=lr)
-    for epoch in range(1, epochs + 1):
+    loss_fn = get_torch_loss(loss_function_name=loss_function_name)
+    rolling_avg_loss = np.inf
+    loss_threshold = 1e-2
+    epochs_avg_losses = []
+    for epoch in tqdm(range(1, epochs + 1)):
+        batch_losses = []
         for i, (X, y) in enumerate(dl):
             optimizer.zero_grad()
             y_hat = model(X)
-            loss = mse_loss_fn(y, y_hat)
+            loss = loss_fn(y, y_hat)
+            batch_losses.append(loss.item())
             loss.backward()
             optimizer.step()
-        if epoch % 10 == 0:
-            print(f'epoch = {epoch}, loss= {loss.item()}')
+
+        epochs_avg_losses.append(np.nanmean(batch_losses))
+        rolling_avg_loss = np.nanmean(epochs_avg_losses[:-10])
+        if epoch % 10 == 0 and epoch > 10:
+            logger.info(f'epoch = {epoch}, rolling-avg-loss= {rolling_avg_loss.item()}')
+        if rolling_avg_loss < loss_threshold:
+            logger.info(f'avg-loss = {rolling_avg_loss}<loss_threshold = {loss_threshold}. Ending the training loop. ')
+            break
+    # compare to baseline
+    print(f'baseline-for dataset {dataset} = {baseline}')
+    print(f'Model {model} : avg loss = {np.nanmean(rolling_avg_loss)}')
--- phd_experiments/datasets/toy_relu.py
+++ phd_experiments/datasets/toy_relu.py
@@ -5,11 +5,11 @@ from torch.utils.data import Dataset
 class ToyRelu(Dataset):
     def __init__(self, N, input_dim, out_dim):
         self.N = N
-        self.data_x = torch.randn((self.N, input_dim))
-        self.data_y = (torch.rand(size=(self.N, out_dim)) < 0.5).float()
+        self.X = torch.randn((self.N, input_dim))
+        self.y = (torch.rand(size=(self.N, out_dim)) < 0.5).float()
 
     def __len__(self):
         return self.N
 
     def __getitem__(self, index):
-        return self.data_x[index], self.data_y[index]
+        return self.X[index], self.y[index]
