ca15a4b600e3309c49f00da6bbcfd38134e1b12c
mbaddar <mbaddar2@gmail.com>
2023-04-19 23:58:39 +0200

    TODO : synth data using ode systems

---
+++ phd_experiments/datasets/ode_system.py
@@ -0,0 +1,9 @@
+# https://tutorial.math.lamar.edu/classes/de/systemsde.aspx
+# https://math.libretexts.org/Bookshelves/Differential_Equations/Differential_Equations_for_Engineers_(Lebl)/3%3A_Systems_of_ODEs/3.1%3A_Introduction_to_Systems_of_ODEs
+# https://www.projectrhea.org/rhea/index.php/Non-Linear_Systems_of_ODEs
+# https://github.com/mlguy101/augmented-neural-odes/commit/fc9f52b7a2f2180bace16ef3dd334f7dc9b62bd1
+# http://math.uchicago.edu/~may/REU2017/REUPapers/Thomas.pdf
+
+# TODO
+#   1. dzdt = A fn([z,t]) http://web.mit.edu/16.90/BackUp/www/pdfs/Chapter6.pdf
+#   2. example systems above
