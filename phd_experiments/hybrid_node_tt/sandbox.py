b72759ebcebae9d806a921a4c94b2976fe597f3d
mbaddar <mbaddar2@gmail.com>
2023-04-20 05:19:47 +0200

    add polyreg model

--- phd_experiments/hybrid_node_tt/sandbox.py
+++ phd_experiments/hybrid_node_tt/sandbox.py
@@ -11,9 +11,9 @@ class ToyData1(Dataset):
     def __init__(self, Dx):
         N = 10000
         self.N = N
-        W = torch.tensor([0.1, 0.2]).view(1, 2)
+        W = torch.tensor([0.1, -0.2, 0.3]).view(1, Dx)
         X = torch.distributions.Normal(0, 1).sample(torch.Size([N, Dx]))
-        X_nl = torch.pow(X, 2)  # torch.sin(X)
+        X_nl = torch.sin(X)
         y = torch.einsum('ij,bj->b', W, X_nl).view(-1, 1)
         self.X = X_nl
         self.Y = y
@@ -36,6 +36,21 @@ class NNmodel(nn.Module):
     def forward(self, x):
         out = self.net(x)
         return out
+
+
+class PolyReg(nn.Module):
+    def __init__(self, in_dim, out_dim, deg):
+        super().__init__()
+        self.deg = deg
+        self.linear_part = torch.nn.Linear(in_features=in_dim * (deg + 1), out_features=out_dim)
+
+    def forward(self, x):
+        x_pows = []
+        for d in range(self.deg + 1):
+            x_pows.append(torch.pow(x, d))
+        x_pows_cat = torch.cat(x_pows, dim=1)
+        y_hat = self.linear_part(x_pows_cat)
+        return y_hat
 
 
 class LinearModel(torch.nn.Module):
@@ -68,26 +83,28 @@ def tt_opt_block(model, X, y, optim, loss_func):
 
 
 if __name__ == '__main__':
-    Dx = 1
+    Dx = 3
     output_dim = 1
     poly_deg = 3
     rank = 3
     #
-    #model = NNmodel(Dx, output_dim)
+    # model = NNmodel(Dx, output_dim)
     # model = LinearModel(in_dim=Dx, out_dim=1)
-    model = TensorTrainFixedRank(dims=[poly_deg + 1] * Dx, fixed_rank=rank, requires_grad=True, unif_low=-0.01,
-                                 unif_high=0.01, poly_deg=poly_deg)
+    # model = TensorTrainFixedRank(dims=[poly_deg + 1] * Dx, fixed_rank=rank, requires_grad=True, unif_low=-0.01,
+    #                              unif_high=0.01, poly_deg=poly_deg)
+    model = PolyReg(in_dim=Dx,out_dim=output_dim,deg=poly_deg)
     loss_fn = nn.MSELoss()
     learning_rate = 0.001
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
     ds = ToyData1(Dx)
     dl = DataLoader(dataset=ds, batch_size=64, shuffle=True)
     epochs = 50000
+    print(f'model type = {type(model)}')
     for epoch in range(epochs):
         # Clear gradients w.r.t. parameters
         losses = []
         for i, (X, y) in enumerate(dl):
-            if isinstance(model, (NNmodel, LinearModel)):
+            if isinstance(model, (NNmodel, LinearModel,PolyReg)):
                 loss_val = nn_opt_block(model=model, X=X, y=y, optim=optimizer, loss_func=loss_fn)
             elif isinstance(model, TensorTrainFixedRank):
                 loss_val = tt_opt_block(model=model, X=X, y=y, optim=optimizer, loss_func=loss_fn)
